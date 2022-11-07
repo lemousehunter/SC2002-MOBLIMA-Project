f763c5dcb38aa03b62063cc5b60fbc30ee8c6f66
AryanSethi20 <aryansethi20@gmail.com>
2022-11-07 18:32:40 +0800

    Update Cineplex.java
    Added javadoc comments

--- SC2002-MOBLIMA-Project/src/Cineplex.java
+++ SC2002-MOBLIMA-Project/src/Cineplex.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * A Cineplex Object
+ * 
+ * <p>
+ * A <code>Cineplex</code> object used to store all the parameters
+ * about a cineplex
+ * </p>
+ * 
+ */
+
 public class Cineplex {
 
 	private String cineplexID;
@@ -9,9 +19,11 @@ public class Cineplex {
 	private ArrayList<Screen> masterScreens;
 
 	/**
-	 * @param cineplexID
-	 * @param name
-	 * @param location
+	 * Cineplex Constructor
+	 * 
+	 * @param cineplexID The unique ID of a cineplex
+	 * @param name The cineplex name
+	 * @param location The location of the cineplex
 	 */
 	public Cineplex(String cineplexID, String name, String location) {
 		
@@ -21,47 +33,113 @@ public class Cineplex {
 		this.screenID=new ArrayList<String>();
 	}
 
+	
+	/** 
+	 * Set method for storing a reference to the master arraylist of Screens
+	 * 
+	 * @param masterScreens The master arraylist of all the screens
+	 */
 	public void setMasterScreens(ArrayList<Screen> masterScreens) {
 		this.masterScreens = masterScreens;
 	}
 
+	
+	/** 
+	 * Method to update the screenIDs array with the screenID of the passed object <em>screen</em>
+	 * 
+	 * @param screen An object of class Screen
+	 */
 	public void addScreenID(Screen screen) {
 		this.screenID.add(screen.getScreenID());
 		masterScreens.add(screen);
 	}
 
+	
+	/** 
+	 * Get method for returning the Cineplex ID
+	 * 
+	 * @return The Cineplex ID
+	 */
 	public String getCineplexID() {
 		return cineplexID;
 	}
 
+	
+	/** 
+	 * Set method for setting the Cineplex ID
+	 * 
+	 * @param cineplexID The Cineplex ID
+	 */
 	public void setCineplexID(String cineplexID) {
 		this.cineplexID = cineplexID;
 	}
 
+	
+	/** 
+	 * Get method for returning the name of the cineplex
+	 * 
+	 * @return The name of the Cineplex
+	 */
 	public String getName() {
 		return name;
 	}
 
+	
+	/** 
+	 * Set method for setting the name of the cineplex
+	 * 
+	 * @param name The name of the Cineplex
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	
+	/** 
+	 * Get method for returning the location of the cineplex
+	 * 
+	 * @return the location of the cineplex
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	
+	/** 
+	 * Set method for setting the location of the cineplex
+	 * 
+	 * @param location The location of the cineplex
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	
+	/** 
+	 * Get method for returning the screen ID
+	 * 
+	 * @return ArrayList<String>
+	 */
 	public ArrayList<String> getScreenID() {
 		return screenID;
 	}
 
+	
+	/** 
+	 * Set method for setting the screenID
+	 * 
+	 * @param screenID An arraylist of Screen IDs
+	 */
 	public void setScreenID(ArrayList<String> screenID) {
 		this.screenID = screenID;
 	}
 
+	
+	/** 
+	 * Method for returning the arraylist of all the screens in the current cineplex
+	 * 
+	 * @return The arraylist of all the screens in the current cineplex
+	 */
 	public ArrayList<String> getScreenNames() {
 		ArrayList<String> screens = new ArrayList<String>();
 		for(String screenid: screenID) {
@@ -75,6 +153,12 @@ public class Cineplex {
 		return screens;
 	}
 
+	
+	/** 
+	 * Method for returning the details of the cineplex for printing on the screen
+	 * 
+	 * @return All the attributes of the cineplex class in a string format
+	 */
 	public String viewDetails() {
 		return "Cineplex cineplexID = " + cineplexID + " , name = " + name + " , location = " + location + " , screens = "
 				+ getScreenNames().toArray().toString();
