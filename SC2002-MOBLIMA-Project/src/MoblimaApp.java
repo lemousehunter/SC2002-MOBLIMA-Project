51c96996513aaac615a8b32199874f05c6be13e4
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2022-11-13 18:13:44 +0800

    Update MoblimaApp.java

--- SC2002-MOBLIMA-Project/src/MoblimaApp.java
+++ SC2002-MOBLIMA-Project/src/MoblimaApp.java
@@ -211,7 +211,7 @@ public class MoblimaApp {
                     movieGoer = this.centralManager.getMovieGoerMgr().getUserByName(userName);
                     if (movieGoer != null) {
                         System.out.println("\n" + userName + "  Already Registered");
-                        this.ManageMovieGoerApp(); // go back to movieGoerMenu
+                        break;
                     }
                     System.out.print("Enter Email ID: ");
                     String emalid = sc.nextLine();
